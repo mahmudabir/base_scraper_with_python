ba509e0fd323699ff53f80199fb97f8a29fd0d66
Abir <abir@primetechbd.com>
2024-04-25 11:39:18 +0600

    launch json

--- helpers/file_download_helper.py
+++ helpers/file_download_helper.py
@@ -2,6 +2,7 @@
 import os
 import time
 from urllib.parse import unquote
+
 import requests
 
 from helpers.app_constants import Color
@@ -22,40 +23,46 @@ def download_file_base(url, file_name=None, directory_name=None):
     Raises:
         None
     """
-    response = requests.get(url, timeout=300, stream=True)
-    total_length = int(response.headers.get("content-length", 0))
-    directory_name = "downloads" if directory_name is None else directory_name
-    file_name = os.path.basename(unquote(url)) if file_name is None else file_name
+    
+    try:
+        response = requests.get(url, timeout=5, stream=True)
+        total_length = int(response.headers.get("content-length", 0))
+        directory_name = "downloads" if directory_name is None else directory_name
+        file_name = os.path.basename(unquote(url)) if file_name is None else file_name
 
-    os.makedirs(directory_name, exist_ok=True)
-    full_file_path = os.path.join(directory_name, file_name)
+        os.makedirs(directory_name, exist_ok=True)
+        full_file_path = os.path.join(directory_name, file_name)
 
-    with open(full_file_path, "wb") as f:
-        dl = 0
-        start_time = time.time()
-        update_time = start_time
+        with open(full_file_path, "wb") as f:
+            dl = 0
+            start_time = time.time()
+            update_time = start_time
 
-        for data in response.iter_content(chunk_size=4096):
-            dl += len(data)
-            f.write(data)
-            elapsed_time = time.time() - start_time
-            download_speed = dl / (1024 * elapsed_time) if elapsed_time > 0 else 0
-            percent_done = dl / total_length * 50 if total_length > 0 else 0
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                elapsed_time = time.time() - start_time
+                download_speed = dl / (1024 * elapsed_time) if elapsed_time > 0 else 0
+                percent_done = dl / total_length * 50 if total_length > 0 else 0
 
-            if time.time() - update_time >= 2:
-                progress_bar_completed = "=" * int(percent_done / 2)
-                progress_bar_remaining = " " * (25 - int(percent_done / 2))
-                downloaded_size = dl / (1024 * 1024)
-                file_size = total_length / (1024 * 1024)
-                print(
-                    f"""{Color.PURPLE}Downloading \"{file_name}\": [{progress_bar_completed}{progress_bar_remaining}] {Color.BLUE}[Progress: {percent_done:.2f}%] {Color.GREEN}[Speed: {download_speed:.3f} KB/s] {Color.YELLOW}[{downloaded_size:.2f} MB / {Color.RED}{file_size:.2f} MB] {Color.RESET}\r""",
-                    end="\r",
-                )
+                if time.time() - update_time >= 2:
+                    progress_bar_completed = "=" * int(percent_done / 2)
+                    progress_bar_remaining = " " * (25 - int(percent_done / 2))
+                    downloaded_size = dl / (1024 * 1024)
+                    file_size = total_length / (1024 * 1024)
+                    print(
+                        f"""{Color.PURPLE}Downloading \"{file_name}\": [{progress_bar_completed}{progress_bar_remaining}] {Color.BLUE}[Progress: {percent_done:.2f}%] {Color.GREEN}[Speed: {download_speed:.3f} KB/s] {Color.YELLOW}[{downloaded_size:.2f} MB / {Color.RED}{file_size:.2f} MB] {Color.RESET}\r""",
+                        end="\r",
+                    )
 
-                update_time = time.time()
+                    update_time = time.time()
 
-    print("Download complete!")
-    print(
-        f'File: "{Color.GREEN}{os.path.dirname(os.path.abspath(__file__))}\\{full_file_path}{Color.RESET}"'
-    )
+        print("Download complete!")
+        print(
+            f'File: "{Color.GREEN}{os.path.dirname(os.path.abspath(__file__))}\\{full_file_path}{Color.RESET}"'
+        )
+    except Exception as ex:
+        # raise ex
+        print(ex.with_traceback(ex.__traceback__))
+    
 
--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def main():
         FileDownloadModel("http://10.16.100.214/iccftps14/iccftps14sasd5/Tv%20Show/Serials%20(Animation)/I%20Am%20Groot%20Season%2002%20(2023)%20Completed/I%20Am%20Groot%20S02E03.mp4"),
         FileDownloadModel("http://10.16.100.214/iccftps14/iccftps14sasd5/Tv%20Show/Serials%20(Animation)/I%20Am%20Groot%20Season%2002%20(2023)%20Completed/I%20Am%20Groot%20S02E04.mp4"),
         FileDownloadModel("http://10.16.100.214/iccftps14/iccftps14sasd5/Tv%20Show/Serials%20(Animation)/I%20Am%20Groot%20Season%2002%20(2023)%20Completed/I%20Am%20Groot%20S02E05.mp4"),
+        FileDownloadModel("https://mcbuild.org/static/images/items/5/e00af2f0691_mcbuild.webp"),
+        FileDownloadModel("https://mcbuild.org/static/images/items/12/ed55b223418_mcbuild.webp"),
+        FileDownloadModel("https://mcbuild.org/static/images/items/12/ff16dc90571_mcbuild.webp"),
+        FileDownloadModel("https://mcbuild.org/static/images/items/12/53ee5161677_mcbuild.webp"),
     ]
 
     download_multiple_file_concurrently(files)
